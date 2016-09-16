1730c050b0be2c44fb0191a2cc3629ea89007820
jidoka1902 <anton_sylvester@msn.com>
2016-09-16 21:21:32 +0200

    Update textarea_widget.html.php
    seems like a copy-paste-error from an input widget,
    no type necessary, $attr['class'] should not get lost since it comes from the formBuilder

--- src/ShopwareSymfonyForms/FormFactory/Resources/views/Form/textarea_widget.html.php
+++ src/ShopwareSymfonyForms/FormFactory/Resources/views/Form/textarea_widget.html.php
@@ -1,7 +1,4 @@
 <?php
-$class = isset($class) ? $class : '';
-if (!isset($type) || 'file' != $type) {
-    $attr['class'] = trim($class . ' ' . $view['form']->block($form, 'form_widget_class') . ' ' . $view['form']->block($form, 'form_error'));
-}
+    $attr['class'] = trim($attr['class'] . ' ' . $view['form']->block($form, 'form_widget_class') . ' ' . $view['form']->block($form, 'form_error'));
 ?>
 <textarea <?php echo $view['form']->block($form, 'widget_attributes', ['attr' => $attr]) ?>><?php echo $view->escape($value) ?></textarea>
